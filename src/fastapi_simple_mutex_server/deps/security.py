504bd4b2c1602fb1737d7d44d5213cddf2416d1d
Ehren <63324222+EhrenLeAwesome@users.noreply.github.com>
2021-07-20 14:57:00 -0500

    use secure string compare :) (#1)
    Co-authored-by: Ehren Simonsmeier <ehren.simonsmeier@wildcardcorp.com>

--- src/fastapi_simple_mutex_server/deps/security.py
+++ src/fastapi_simple_mutex_server/deps/security.py
@@ -4,7 +4,7 @@ from fastapi import Security
 from fastapi.security.api_key import APIKeyQuery
 
 from fastapi_simple_mutex_server.config import service_config
-
+from secrets import compare_digest as secure_strings_same
 
 NotAuthenticated403 = lambda: HTTPException(
     status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
@@ -13,5 +13,5 @@ NotAuthenticated403 = lambda: HTTPException(
 
 class APIKeySecurity:
     def __init__(self, api_key: str = Security(APIKeyQuery(name='api_key'))):
-        if not api_key == service_config.api_key:
+        if not secure_strings_same(api_key, service_config.api_key):
             raise NotAuthenticated403()
